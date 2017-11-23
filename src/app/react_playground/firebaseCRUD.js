b959e3da2539500f6c832735544cbe5f7b413e02
twistezo <twistezo7@gmail.com>
2017-11-23 19:11:15 +0100

    fireabase CRUD #2

--- src/app/react_playground/firebaseCRUD.js
+++ src/app/react_playground/firebaseCRUD.js
@@ -3,46 +3,25 @@ import database from '../firebase';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
-const usersRef = database.ref('users/');
-
-// this function add new user if not exists
-// or update fields if userEmail exists
-function addUser(userEmail, userName) {
-  const newPostKey = usersRef.push().key;
-  usersRef.child(userEmail).set({
-    id: newPostKey,
-    email: userEmail,
-    name: userName
-  });
-}
-
-// TODO:
-function readUserByUserEmail(userEmail) {
-
-}
-
-// TODO:
-function removeUserByUserEmail(userEmail) {
-
-}
-
-// FIXME: check
-function readAllUsers() {
-  usersRef.orderByKey().on("child_added", function (snapshot) {
-    console.log(snapshot.key + ' ' + snapshot.val().name);
-  });
-}
-
 module.exports = class FirebaseCRUD extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       userEmail: '',
-      userName: ''
+      userName: '',
+      user: {},
+      users: {}
     };
+
+    this.usersRef = database.ref('users/');
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  componentDidMount() {
+    this.readUserByEmail('zxc');
+    this.readAllUsers();
   }
 
   handleChange(event) {
@@ -50,13 +29,62 @@ module.exports = class FirebaseCRUD extends React.Component {
   }
 
   handleSubmit(event) {
-    const userEmail = this.state.userEmail;
-    const userName = this.state.userName;
-    addUser(userEmail, userName);
+    this.addUser(this.state.userEmail, this.state.userName);
     event.preventDefault();
   }
 
-  // TODO: lists of users
+  // adds new user if not exists
+  // or update user fields if userEmail exists
+  addUser(userEmail, userName) {
+    const newPostKey = this.usersRef.push().key;
+    this.usersRef.child(userEmail).set({
+      id: newPostKey,
+      email: userEmail,
+      name: userName
+    });
+  }
+
+  readUserByEmail(userEmail) {
+    let user = {};
+    this.usersRef.child(userEmail).on('value', snapshot => {
+      user = {
+        id: snapshot.val().id,
+        email: snapshot.val().email,
+        name: snapshot.val().name
+      };
+      this.setState({ user });
+    });
+  }
+
+  removeUserByEmail(userEmail) {
+    this.usersRef.child(userEmail).remove();
+  }
+
+  // FIXME:
+  readAllUsers() {
+    const users = [];
+    this.usersRef.orderByKey().on('child_added', snapshot => {
+      const user = snapshot.val();
+      users.push(user);
+    });
+    console.log(users);
+    // this.setState({ users });  // FIXME:
+  }
+
+  UserList() {
+    // const users = this.state.users;
+    // console.log(users);
+    const users = [1, 2, 3, 4, 5];
+    const listItems = users.map((user) =>
+      <li key={user}>
+        {user}
+      </li>
+    );
+    return (
+      <ul>{listItems}</ul>
+    );
+  }
+
   render() {
     return (
       <div className="col-md-6 mx-auto">
@@ -86,6 +114,14 @@ module.exports = class FirebaseCRUD extends React.Component {
           <p>
             userName = {this.state.userName}
           </p>
+          <p>
+            readUserByEmail() = id: {this.state.user.id}, email: {this.state.user.email}, name: {this.state.user.name}
+          </p>
+        </div>
+        <div className="pb-2">
+          {/* {this.state.users[0].email} */}
+          {/* {this.state.users[1].name} */}
+          {/* <this.UserList /> */}
         </div>
       </div >
     );
