a81183e06d78243ac1d499f94c76cdc38f344e60
twistezo <twistezo7@gmail.com>
2017-12-11 22:24:05 +0100

    resolve

--- src/app/react_playground/firebase-auth.js
+++ src/app/react_playground/firebase-auth.js
@@ -1,25 +1,68 @@
 import React from 'react';
-import { database, auth, provider } from '../firebase';
+import auth from '../firebase';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
 class FirebaseAuth extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      loggedIn: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleLogin() {
+    auth.signInWithEmailAndPassword(this.state.email, this.state.password).then(() => {
+      this.setState({
+        loggedIn: true
+      });
+    });
+  }
+
+  handleLogout() {
+    auth.signOut().then(() => {
+      this.setState({
+        loggedIn: false
+      });
+    });
+  }
 
   render() {
     return (
       <div className="col-md-4 mx-auto text-center pt-5">
         <h2>{'firebase-auth testing'}</h2>
-        <form>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        <span>{'Test with email: test@gmail.com pass: qwetest'}</span>
+
+        <div className="form-group">
+          <label>Email address</label>
+          <input value={this.state.email} onChange={this.handleChange} name="email" className="form-control" placeholder="Email" />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input value={this.state.password} onChange={this.handleChange} name="password" className="form-control" placeholder="Password" />
+        </div>
+        {this.state.loggedIn ?
+          <button onClick={this.handleLogout} type="submit" className="btn btn-primary">Log out</button>
+          :
+          <button onClick={this.handleLogin} type="submit" className="btn btn-primary">Log in</button>
+        }
+
+        <div>
+          <h4>Are you loggedin ?</h4>
+          {this.state.loggedIn ?
+            <p>{'You are logged'}</p> : <p>{'You are not logged in'}</p>
+          }
+        </div>
       </div>
     );
   }
