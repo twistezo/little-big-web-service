641dd1ed644ab8c8ba87c9c2c13df027b7a4e49a
twistezo <twistezo7@gmail.com>
2018-02-07 15:45:02 +0100

    Fixed bootstrap postcss-loader error and fixed react router navigation

--- client/src/app/components/layout.js
+++ client/src/app/components/layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {
-    BrowserRouter as Router,
-    Route,
-    Link
-} from 'react-router-dom'
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Redirect
+} from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
@@ -14,33 +15,42 @@ import FirebaseCRUD from './firebaseCRUD';
 import FirebaseAuth from './firebaseAuth';
 
 const TabsNavbar = () => (
-    <Router>
-        <div>
-            <ul className="nav nav-tabs justify-content-center">
-                <li className="nav-item"><Link className="nav-link active" to="/">Bootstrap</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/react">React</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/firebaseCRUD">Firebase CRUD</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/FirebaseAuth">Firebase Auth</Link></li>
-            </ul>
-            <Route exact path="/" component={PlaygroundBootstrap} />
-            <Route path="/react" component={PlaygroundInheritance} />
-            <Route path="/firebaseCRUD" component={FirebaseCRUD} />
-            <Route path="/FirebaseAuth" component={FirebaseAuth} />
-        </div>
-    </Router>
-)
+  <Router>
+    <div>
+      <ul className="nav nav-tabs justify-content-center">
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/bootstrap">Bootstrap</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/react">React</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/firebaseCRUD">Firebase CRUD</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/FirebaseAuth">Firebase Auth</NavLink>
+        </li>
+      </ul>
+      <Route exact path="/bootstrap" component={PlaygroundBootstrap} />
+      <Route path="/react" component={PlaygroundInheritance} />
+      <Route path="/firebaseCRUD" component={FirebaseCRUD} />
+      <Route path="/FirebaseAuth" component={FirebaseAuth} />
+      <Redirect from="/" to="/bootstrap" />
+    </div>
+  </Router>
+);
 
 class Layout extends React.Component {
-    render() {
-        return (
-            <div className="container pt-5">
-                <div className="pb-5 text-center">
-                    <h2>Little Big Web Service - playground</h2>
-                </div>
-                <TabsNavbar />
-            </div >
-        );
-    }
+  render() {
+    return (
+      <div className="container pt-5">
+        <div className="pb-5 text-center">
+          <h2>Little Big Web Service - playground</h2>
+        </div>
+        <TabsNavbar />
+      </div >
+    );
+  }
 }
 
 export default Layout;
